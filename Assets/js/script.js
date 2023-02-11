1fafac8e421fe75bb0f544398a1da03044599ece
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-11 14:17:20 -0500

    added generate password function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// function generateBtn() {
-//   let passwordLength = prompt("Enter password length (minimum 8 characters):");
-//   if (passwordLength < 8) {
-//     alert("Password length must be at least 8 characters");
-//     else if (passwordLength > 100) {
-//       alert("Password length must be at most 100 characters");
-//       return;
-//     };
-//   };
-//   }
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password();
-generatePassword;
+  passwordText.value = password;
 }
+
+//Generate password function
+function generatePassword() {
+  let passwordLength = prompt("Enter password length (minimum 8 characters):");
+  if (passwordLength < 8) {
+    alert("Password length must be at least 8 characters");
+  if else (passwordLength > 100)
+      alert("Password length must be at most 100 characters");
+  return;
+    };
+  }
 
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
