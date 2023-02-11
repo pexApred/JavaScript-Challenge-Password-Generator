fd5a42ebf9ceab4a534f3f76c84aef4ed31b7d57
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-11 14:53:38 -0500

    writing code to center content on page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Center content code
+var div = document.getElementsByClassName("wrapper");
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,15 +16,20 @@ function writePassword() {
 //Generate password function
 function generatePassword() {
   let passwordLength = prompt("Enter password length (minimum 8 characters):");
-  passwordLength = Number(passwordLength)
+  passwordLength = Number(passwordLength);
+
   if (passwordLength < 8) {
     alert("Password length must be at least 8 characters");
-
-  else if (passwordLength > 100)
-      alert("Password length must be at most 100 characters");
-  return;
-    };
+    return;
   }
+  else if (passwordLength > 100) {
+    alert("Password length must be at most 100 characters");
+    return;
+  } console.log(generatePassword);
+
+
+}
+
 
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
