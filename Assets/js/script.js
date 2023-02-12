a52f920a5300d3e393b6812cd83ff46c15e83992
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-12 13:43:07 -0500

    added if statements for character types

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,8 +10,9 @@ function writePassword() {
 }
 
 // NOTE to self: PROMPT function displays a model dialog with a message, input field, & 2 buttons "OK" = returns or "CANCEL" = null
-// NOTE to self: ALERT function
-// Generate password function
+// NOTE to self: ALERT function displays a model dialog with a message and an "OK" button
+
+// Generate password function : Password Criteria
 function generatePassword() {
   let passwordLength = prompt("Enter password length (minimum 8 characters):");
   passwordLength = Number(passwordLength);
@@ -26,11 +27,22 @@ function generatePassword() {
   } console.log(generatePassword);
 
   // NOTE to self: CONFIRM function is used to display dialog box with a message & 2 buttons "OK" = TRUE or "CANCEL" = FALSE
+
   let includeLowercase = confirm("Should I include lowercase characters?");
   let includeUppercase = confirm("Should I include uppercase characters?");
   let includeNumbers = confirm("Should I include numerical characters?");
   let includeSpecial = confirm("Should I include special characters?");
 
+  let charTypes = [];
+
+  // NOTE to self: PUSH 
+
+  if (includeLowercase) charTypes.push("abcdefghijklmnopqrstuvwxyz");
+  if (includeUppercase) charTypes.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  if (includeNumbers) charTypes.push("1234567890");
+  if (includeSpecial) charTypes.push(" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+  
+  
 }
 
 
