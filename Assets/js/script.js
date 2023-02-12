f87c773eb1c136cfbe6d60008cf790fc87b6361e
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-12 13:57:30 -0500

    fixed README image path & added notes in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,9 +14,9 @@ function writePassword() {
 
 // Generate password function : Password Criteria
 function generatePassword() {
+  // Prompt for length of Password
   let passwordLength = prompt("Enter password length (minimum 8 characters):");
   passwordLength = Number(passwordLength);
-
   if (passwordLength < 8) {
     alert("Password length must be at least 8 characters");
     return;
@@ -25,24 +25,23 @@ function generatePassword() {
     alert("Password length must be at most 128 characters");
     return;
   } console.log(generatePassword);
-
   // NOTE to self: CONFIRM function is used to display dialog box with a message & 2 buttons "OK" = TRUE or "CANCEL" = FALSE
-
+  // Confirms for character types
   let includeLowercase = confirm("Should I include lowercase characters?");
   let includeUppercase = confirm("Should I include uppercase characters?");
   let includeNumbers = confirm("Should I include numerical characters?");
   let includeSpecial = confirm("Should I include special characters?");
 
   let charTypes = [];
-
-  // NOTE to self: PUSH 
-
+  // NOTE to self: PUSH is an array method to add elements to end of array
   if (includeLowercase) charTypes.push("abcdefghijklmnopqrstuvwxyz");
   if (includeUppercase) charTypes.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   if (includeNumbers) charTypes.push("1234567890");
   if (includeSpecial) charTypes.push(" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
-  
-  
+  // Validation Alert
+  if (charTypes.length === 0) {
+    alert("At least one character type must be selected")
+  }
 }
 
 
--- README.md
+++ README.md
@@ -38,7 +38,7 @@ THEN the password is either displayed in an alert or written to the page
 
 The following image shows the web application's appearance and functionality:
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+![The Password Generator application displays a red button to "Generate Password".](./Assets/Images/03-javascript-homework-demo.png)
 
 ## Grading Requirements
 
