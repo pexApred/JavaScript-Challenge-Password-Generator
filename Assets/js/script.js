c8125e5cbd8e1808a11c0cc12e03f6c952782980
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-12 13:20:17 -0500

    declared variables for character types

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,7 +9,9 @@ function writePassword() {
   passwordText.value = password;
 }
 
-//Generate password function
+// NOTE to self: PROMPT function displays a model dialog with a message, input field, & 2 buttons "OK" = returns or "CANCEL" = null
+// NOTE to self: ALERT function
+// Generate password function
 function generatePassword() {
   let passwordLength = prompt("Enter password length (minimum 8 characters):");
   passwordLength = Number(passwordLength);
@@ -18,11 +20,16 @@ function generatePassword() {
     alert("Password length must be at least 8 characters");
     return;
   }
-  else if (passwordLength > 100) {
-    alert("Password length must be at most 100 characters");
+  else if (passwordLength > 128) {
+    alert("Password length must be at most 128 characters");
     return;
   } console.log(generatePassword);
 
+  // NOTE to self: CONFIRM function is used to display dialog box with a message & 2 buttons "OK" = TRUE or "CANCEL" = FALSE
+  let includeLowercase = confirm("Should I include lowercase characters?");
+  let includeUppercase = confirm("Should I include uppercase characters?");
+  let includeNumbers = confirm("Should I include numerical characters?");
+  let includeSpecial = confirm("Should I include special characters?");
 
 }
 
